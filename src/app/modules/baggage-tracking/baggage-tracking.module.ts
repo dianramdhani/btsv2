58a0998bd08c72ec86ffda1fd4cf4d67d02e2ff7
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-01-30 17:30:59 +0700

    update baggage-tracking: add wizard

--- src/app/modules/baggage-tracking/baggage-tracking.module.ts
+++ src/app/modules/baggage-tracking/baggage-tracking.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { BaggageTrackingRoutingModule } from './baggage-tracking-routing.module';
 import { BaggageTrackingComponent } from './page/baggage-tracking/baggage-tracking.component';
+import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
   declarations: [BaggageTrackingComponent],
   imports: [
     CommonModule,
-    BaggageTrackingRoutingModule
+    BaggageTrackingRoutingModule,
+    SharedModule
   ]
 })
 export class BaggageTrackingModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ArchwizardModule } from 'angular-archwizard';
 
 
 
@@ -7,6 +8,9 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ArchwizardModule
   ]
 })
 export class SharedModule { }
