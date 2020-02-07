d36693e27ca6a43bfcfe0857ebf63db4c7670e85
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-02-07 15:17:21 +0700

    update check-in: finish service

--- src/app/modules/check-in/page/check-in/check-in.component.ts
+++ src/app/modules/check-in/page/check-in/check-in.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 import { CheckinService } from '@data/service/checkin.service';
 
 @Component({
@@ -12,37 +12,57 @@ export class CheckInComponent implements OnInit {
   showNextForm = false;
 
   constructor(
-    private checkInService: CheckinService
+    private checkInService: CheckinService,
   ) { }
 
   ngOnInit() {
     this.formCheckIn = new FormGroup({
       passengerId: new FormControl('', Validators.required),
       airport: new FormControl({ value: '', disabled: true }, Validators.required),
-      baggageId: new FormControl('tes3', Validators.required),
-      baggageTrackingId: new FormControl('tes4', Validators.required),
       location: new FormControl({ value: 'CHECK IN', disabled: true }, Validators.required),
       locationTime: new FormControl(''),
-      trackingDeviceId: new FormControl('99', Validators.required),
-      weight: new FormControl('99', Validators.required),
+      baggages: new FormArray([this.createBaggage()], Validators.required),
     });
-
     this.formCheckIn.get('passengerId').valueChanges.subscribe(() => this.showNextForm = false);
   }
 
-  async checkPassanger() {
-    const { passengerId } = this.formCheckIn.value,
-      passenger = await this.checkInService.getPassengerById(passengerId).toPromise(),
-      code = await this.checkInService.generateCode('CHECK IN', passenger.flight.airportFrom).toPromise();
+  private createBaggage() {
+    return new FormGroup({
+      weight: new FormControl(null, Validators.required),
+      baggageId: new FormControl({ value: '', disabled: true }, Validators.required),
+      baggageTrackingId: new FormControl({ value: '', disabled: true }, Validators.required),
+      trackingDeviceId: new FormControl({ value: '', disabled: true }, Validators.required)
+    });
+  }
 
+  addBaggage() {
+    const baggages = this.formCheckIn.get('baggages') as FormArray;
+    baggages.push(this.createBaggage());
+  }
+
+  removeBaggage(index: number, event: Event = null) {
+    const baggages = this.formCheckIn.get('baggages') as FormArray;
+    baggages.removeAt(index);
+  }
+
+  async checkPassanger() {
+    const { passengerId } = this.formCheckIn.getRawValue(),
+      passenger = await this.checkInService.getPassengerById(passengerId).toPromise();
     this.formCheckIn.patchValue({
-      airport: passenger.flight.airportFrom,
+      airport: passenger.flight.airportFrom
+    });
+    this.showNextForm = true;
+  }
+
+  async generateCode(index: number, event: Event) {
+    const { airport } = this.formCheckIn.getRawValue(),
+      code = await this.checkInService.generateCode('CHECK IN', airport).toPromise(),
+      baggages = this.formCheckIn.get('baggages') as FormArray;
+    baggages.at(index).patchValue({
       baggageId: code.baggageBarcodeId,
       baggageTrackingId: code.baggageTagId,
       trackingDeviceId: code.trackingDevice.id
     });
-
-    this.showNextForm = true;
   }
 
   async submit() {
@@ -52,23 +72,25 @@ export class CheckInComponent implements OnInit {
     const {
       passengerId,
       airport,
-      baggageId,
-      baggageTrackingId,
       location,
       locationTime,
-      trackingDeviceId,
-      weight
-    } = this.formCheckIn.value;
-    const res = await this.checkInService.baggageCheckIn(
-      passengerId,
-      airport,
-      baggageId,
-      baggageTrackingId,
-      location,
-      locationTime,
-      trackingDeviceId,
-      weight
-    ).toPromise();
-    console.log(res);
+      baggages
+    } = this.formCheckIn.getRawValue();
+
+    for (const index in baggages) {
+      const res = await this.checkInService.baggageCheckIn(
+        passengerId,
+        airport,
+        baggages[index].baggageId,
+        baggages[index].baggageTrackingId,
+        location,
+        locationTime,
+        baggages[index].trackingDeviceId,
+        baggages[index].weight
+      ).toPromise();
+    }
+
+    alert('Check In Success');
+    this.formCheckIn.reset();
   }
 }
